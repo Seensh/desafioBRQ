1e880b517a5a0a545fa9a48552b2d3dac758a36e
Seensh <felipe.barbosa001@hotmail.com>
2024-01-18 00:06:15 -0300

    add arquivos faltantes

---
+++ src/main/java/com/desafiobrq/DesafiobrqApplication.java
@@ -0,0 +1,13 @@
+package com.desafiobrq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiobrqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiobrqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiobrq/DesafiobrqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiobrq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiobrqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
